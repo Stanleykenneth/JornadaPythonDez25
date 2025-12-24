f9522674571c49a2fe8fe7aed80c55c884eeaa64
Kenneth Stanley <kstanley_bass@hotmail.com>
2025-12-24 17:58:45 -0300

    Aula004 - Lista, Tuplas e Dicionário

---
+++ aula004/lista.py
@@ -0,0 +1,11 @@
+# Estrutura de dados  Aula 15/12/2025
+
+nomes = ['Joao', 'Maria', 'Jose']
+numeros = [10, 52, 41, 67]
+
+print(nomes)
+print(numeros)
+print("")
+print(nomes[1])
+print(numeros[2])
+print(nomes[-1])
